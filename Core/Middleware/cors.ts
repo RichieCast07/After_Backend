4436426c53c630ecb62c94e279cd713a0c6426a0
Miguel Angel Gutierrez Gomez <233382@ids.upchiapas.edu.mx>
2026-03-05 18:14:40 -0600

    Added: cors

--- Core/Middleware/cors.ts
+++ Core/Middleware/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || ['http://localhost:5173', 'http://localhost:5000', 'http://localhost:8100'],
+  origin: process.env.CORS_ORIGIN || ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:5000', 'http://localhost:8100'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],

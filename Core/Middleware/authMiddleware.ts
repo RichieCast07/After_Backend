4e2d2569ac55325e907f16cfffe0f630198fcbed
Miguel Angel Gutierrez Gomez <233382@ids.upchiapas.edu.mx>
2026-03-22 00:29:27 -0600

    Added: cors valitation

--- Core/Middleware/authMiddleware.ts
+++ Core/Middleware/authMiddleware.ts
@@ -1,5 +1,5 @@
+import type { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import type { Request, Response, NextFunction } from 'express';
 
 // Extendemos la interfaz Request para que reconozca "req.user"
 export interface AuthRequest extends Request {
--- Core/Middleware/cors.ts
+++ Core/Middleware/cors.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || ['http://localhost:5173', "https://after.mangelg.space"],
+  origin: ['http://localhost:5173', "https://after.mangelg.space"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
